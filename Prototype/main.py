28958f1af7794794cb1cd2f1485ee6c67e43dda2
LemonPhase <95856264+LemonPhase@users.noreply.github.com>
2022-07-31 22:32:07 +0800

    Ace adaptation

--- Prototype/main.py
+++ Prototype/main.py
@@ -4,6 +4,7 @@ RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 SUITS = ["Spades", "Hearts", "Clubs", "Diamonds"]
 RANK_VALUES = {"A": 11, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
                "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10}
+
 print(RANKS)
 
 
@@ -25,6 +26,13 @@ class Deck:
         self.cards = []
         self.dealt_cards = []
 
+    def __repr__(self) -> str:
+        deck_comp = ""
+        for card in self.cards:
+            deck_comp += "\n" + card.__str__()
+        return f"The deck has: {deck_comp}"
+
+    # No. of player +1 new set of cards
     def create_deck(self):
         for i in range(self.set_of_cards):
             for suit in SUITS:
@@ -32,37 +40,70 @@ class Deck:
                     self.cards.append(Card(rank, suit, RANK_VALUES[rank]))
 
     def shuffle(self):
-        cards = self.cards.copy()
-        self.cards = random.choices(cards, k=len(cards))
+        random.shuffle(self.cards)
 
-    def deal_card(self):
+    def deal(self):
         card = self.cards.pop(0)
         self.dealt_cards.append(card)
         return card
 
 
 class Player:
-    def __init__(self, user_name, id, token) -> None:
-        self.user_name = user_name
+    def __init__(self, name, id, token) -> None:
+        self.name = name
         self.id = id
-        self.__token = token
+        self.__token = int(token)
         self.hand = []
-
-    def count(self):
-        count = 0
+        self.value = 0
+        self.aces = 0
+    
+    def __repr__(self) -> str:
+        return f"{self.name} has {len(self.hand)} cards."
+    
+    def add(self, card):
+        self.hand.append(card)
+        self.value += card.value
+    
+    def cal_value(self):
+        self.value = 0
         for card in self.hand:
-            if type(card) == int:
-                count += card
+            self.value += card.value
+        return self.value
+
+    
+    def ace(self):
+        for card in self.hand:
+            if card.rank == 'A':
+                card.value = 1
+            break
+        self.cal_value()
 
 
-# Test
-Deck1 = Deck()
-Deck1.create_deck()
-Deck1.shuffle()
 
-for card in Deck1.cards:
-    print(card)
-print("--------------------------------------------------------------------------")
-print(Deck1.deal_card())
-print(Deck1.deal_card())
-print(Deck1.dealt_cards)
+
+# Test deck, shuffle, player and deal
+test_deck = Deck()
+test_deck.create_deck()
+test_player1 = Player("Bob", "123", 500)
+test_player2 = Player("Amy", "456", 100)
+for i in range(2):
+    test_player1.add(test_deck.deal())
+    test_player2.add(test_deck.deal())
+
+print(test_player1)
+print(test_player2)
+print(test_player1.hand)
+print(test_player1.value)
+print(test_player2.hand)
+print(test_player2.value)
+
+# Test ace value adaptation
+for card in test_player1.hand:
+    if card.rank == "A":
+        test_player1.add(test_deck.deal())
+    if test_player1.cal_value() > 21:
+        test_player1.ace()
+
+print(test_player1.hand)
+print(test_player1.value)
+    
