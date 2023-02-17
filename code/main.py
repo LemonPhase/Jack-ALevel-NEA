931fe358ae660252e6c28104f37566a6d766c44a
LemonPhase <lemonphase.zzz@gmail.com>
2023-02-17 09:57:58 +0800

    Some clean up

--- code/main.py
+++ code/main.py
@@ -4,7 +4,6 @@ import os
 import cv2
 from player import Player
 from alien import Ax, Eldredth
-from hand import HandDetector
 
 # https://www.youtube.com/watch?v=o-6pADy5Mdg&t=108s (Thank you for saving my life, ily)
 
@@ -14,7 +13,7 @@ os.chdir(os.path.dirname(__file__))
 
 class Game:
     def __init__(self) -> None:
-        # Hand detector setup
+        # Camera setup
         self.capture = cv2.VideoCapture(0)
         self.has_capture = True
         if self.capture == None or not self.capture.isOpened():
@@ -41,6 +40,21 @@ class Game:
             return img
         else:
             return None
+
+    def calculate_fps(self, img):
+        self.current_time = pygame.time.get_ticks() / 1000
+        fps = 1 / (self.current_time - self.previous_time)
+        self.previous_time = self.current_time
+        cv2.putText(
+            img,
+            str(int(fps)),
+            (10, 70),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            3,
+            (255, 0, 255),
+            3,
+        )
+        cv2.imshow("Image", img)
 
     def alien_spawn(self, type):
         if type == "Ax":
@@ -72,38 +86,22 @@ class Game:
 
         img = self.cam_capture()
 
-        if self.has_capture:
-            self.player.update(img, self.has_capture)
-        else:
-            self.player.update(img, self.has_capture)
+        self.player.update(img, self.has_capture)
 
         self.player.sprite.lasers.draw(screen)
+        self.player.draw(screen)
+
         self.aliens.update()
+        self.aliens.draw(screen)
         for alien in self.aliens.sprites():
             alien.lasers.draw(screen)
 
         self.collision_check()
-        self.player.draw(screen)
 
         # Calculate FPS
-        self.current_time = pygame.time.get_ticks() / 1000
-        fps = 1 / (self.current_time - self.previous_time)
-        self.previous_time = self.current_time
-        self.aliens.draw(screen)
 
         if self.has_capture:
-            cv2.putText(
-                img,
-                str(int(fps)),
-                (10, 70),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                3,
-                (255, 0, 255),
-                3,
-            )
-            cv2.imshow("Image", img)
-
-        pass
+            self.calculate_fps(img)
 
 
 if __name__ == "__main__":
