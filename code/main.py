1d824dcfd0a45bfcd56e8897c11bf45cbe9a805a
LemonPhase <95856264+LemonPhase@users.noreply.github.com>
2023-02-21 21:08:39 +0800

    Timer works with pause menu and new game
    Also added some comments for readability

--- code/main.py
+++ code/main.py
@@ -25,6 +25,10 @@ class Game:
         except:
             print("Error: no capture")
 
+        # Play time record
+        self.start_time = pygame.time.get_ticks()
+        self.play_time = 0
+
         # FPS
         self.clock = pygame.time.Clock()
         self.previous_time = 0
@@ -61,7 +65,8 @@ class Game:
         else:
             return None
 
-    def get_font(self, size):  # Returns Press-Start-2P in the desired size
+    def get_font(self, size):
+        # Returns font in the desired size
         return pygame.font.Font("..\Font\Brandford.otf", size)
 
     def calculate_fps(self, img):
@@ -80,6 +85,7 @@ class Game:
         cv2.imshow("Image", img)
 
     def calculate_cd(self, time):
+        # the respawn cd decreases as time passes, minimum cd 0.5s
         return math.exp(-1 * (1 / 30 * time - 1)) + 1 / 2
 
     def alien_spawn(self, type):
@@ -124,8 +130,6 @@ class Game:
                 if pygame.sprite.spritecollide(alien, self.player, False):
                     alien.kill()
                     self.lives -= 1
-            if self.lives <= 0:
-                self.game_over()
 
     def display_lives(self):
         for live in range(self.lives):
@@ -143,28 +147,40 @@ class Game:
 
     def display_time(self):
         time_surf = self.get_font(50).render(
-            str(round(pygame.time.get_ticks() / 1000, 1)), False, "white")
+            str(round((self.play_time + pygame.time.get_ticks() - self.start_time) / 1000, 1)), False, "white")
         time_rect = time_surf.get_rect(
             topright=(SCREEN_WIDTH-15, 10))
         screen.blit(time_surf, time_rect)
 
-    def game_over(self):
-        print("Game over!")
+    def quit_game(self):
+        print("Game quit")
         pygame.quit()
         sys.exit()
 
     def pause_menu(self):
+        # Pause play time
+
+        self.play_time += pygame.time.get_ticks() - self.start_time
+
         pygame.display.set_caption("Game paused")
         running = True
+
+        # Pause menu loop
         while running:
+            # Pause the timer
+            self.start_time = pygame.time.get_ticks()
             screen.fill((10, 10, 10))
+
+            # Mouse position
             menu_mouse_pos = pygame.mouse.get_pos()
 
+            # Menu title
             menu_text = self.get_font(100).render(
                 "Pause Menu", True, (64, 192, 225))
-
             menu_rect = menu_text.get_rect(center=(SCREEN_WIDTH/2, 150))
+            screen.blit(menu_text, menu_rect)
 
+            # Button setup
             continue_button = Button(pos=(SCREEN_WIDTH/2, 325), text_input="CONTINUE",
                                      font=self.get_font(75), base_color=(196, 252, 192), hovering_color="white")
             main_menu_button = Button(pos=(SCREEN_WIDTH/2, 450), text_input="MAIN MENU",
@@ -172,23 +188,23 @@ class Game:
             quit_button = Button(pos=(SCREEN_WIDTH/2, 575), text_input="QUIT",
                                  font=self.get_font(75), base_color=(196, 252, 192), hovering_color="white")
 
-            screen.blit(menu_text, menu_rect)
-
+            # Display button
             for button in [continue_button, main_menu_button, quit_button]:
                 button.change_color(menu_mouse_pos)
                 button.update(screen)
 
+            # Button clicks
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                    self.game_over()
+                    self.quit_game()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if continue_button.check_input(menu_mouse_pos):
                         return True
                     if main_menu_button.check_input(menu_mouse_pos):
                         return False
                     if quit_button.check_input(menu_mouse_pos):
-                        self.game_over()
+                        self.quit_game()
 
             pygame.display.update()
 
@@ -219,12 +235,20 @@ class Game:
         if self.has_capture:
             self.calculate_fps(img)
 
+        # Game over
+        if self.lives <= 0:
+            return False
+        else:
+            return True
+
     def play(self):
         bg_img = pygame.image.load("..\Graphics\Background.jpg")
         bg_img = pygame.transform.scale(bg_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
         i = 0
         running = True
+
+        # Gameplay loop
         while running:
 
             # Background scrolling
@@ -236,33 +260,45 @@ class Game:
                 i = 0
             i += 3  # Pixel each scroll
 
+            running = self.run()
+            pygame.display.flip()
+            self.clock.tick(TARGET_FPS)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                    self.game_over()
+                    self.quit_game()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         running = self.pause_menu()
 
-            self.run()
-            pygame.display.flip()
-            self.clock.tick(60)
-
 
 def main_menu():
+    # First game
     game = Game()
     pygame.display.set_caption("Main Menu")
     running = True
     new_game = False
+
+    # Loop logic:
+    # Main menu loop > game loop > pause menu loop
+
+    # Main menu loop
     while running:
+        # Background
         screen.fill((10, 10, 10))
+
+        # Mouse position
         menu_mouse_pos = pygame.mouse.get_pos()
 
+        # Title
         menu_text = game.get_font(150).render(
             "GALAXY KNIGHT", True, (64, 192, 225))
 
         menu_rect = menu_text.get_rect(center=(SCREEN_WIDTH/2, 150))
+        screen.blit(menu_text, menu_rect)
 
+        # Button setup
         play_button = Button(pos=(SCREEN_WIDTH/2, 325), text_input="PLAY",
                              font=game.get_font(100), base_color=(196, 252, 192), hovering_color="white")
         leader_board_button = Button(pos=(SCREEN_WIDTH/2, 450), text_input="LEADER BOARD",
@@ -270,28 +306,28 @@ def main_menu():
         quit_button = Button(pos=(SCREEN_WIDTH/2, 575), text_input="QUIT",
                              font=game.get_font(100), base_color=(196, 252, 192), hovering_color="white")
 
-        screen.blit(menu_text, menu_rect)
-
+        # Display button
         for button in [play_button, leader_board_button, quit_button]:
             button.change_color(menu_mouse_pos)
             button.update(screen)
 
+        # Button clicks
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                game.game_over()
+                game.quit_game()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if play_button.check_input(menu_mouse_pos):
                     game.play()
-                    # Game finished
+                    # Previous game finished
                     new_game = True
                 if quit_button.check_input(menu_mouse_pos):
-                    game.game_over()
+                    game.quit_game()
 
+        # Create a new game
         if new_game:
             # Instantiating new game object, creating a new game
             game = Game()
-            print("new game")
             new_game = False
 
         pygame.display.update()
@@ -303,5 +339,6 @@ if __name__ == "__main__":
     SCREEN_WIDTH = 1080
     PLAYER_SPEED = 10
     PLAYER_SIZE = (60, 60)
+    TARGET_FPS = 45
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     main_menu()
